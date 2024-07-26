6d4d650aa5fd1641e7a9f46cb4c23c3230b180a6
Marco Cruz <marcobouyaa@gmail.com>
2024-07-26 01:32:26 -0500

    entregable-2

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection'); 
+   // En Mayúsculas y singular      // en minúsculas y singular
+const User = sequelize.define('users', {
+    // Definimos las columnas aquí
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+    
+});
+
+module.exports = User;
